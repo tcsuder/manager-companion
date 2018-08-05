9654d7f8e73b88e7453aa969779e30cb45a64ceb
tcsuder@gmail.com <tcsuder@gmail.com>
2018-08-05 14:53:23 -0700

    try out gh-pages solution

--- server.js
+++ server.js
@@ -1,8 +1,4 @@
 const express = require('express');
 const app = express();
 app.use(express.static(__dirname + '/build/'));
-
-app.listen(process.env.PORT || 8080, function(){
-  console.log('THIS IS A LOG FORM SERVER');
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+app.listen(process.env.PORT || 8080);
